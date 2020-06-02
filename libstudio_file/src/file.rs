fe91030f2bf522ea1f45c840edb9633a3da8e225
minerva <l.lehning@web.de>
2020-06-02 22:22:32 +0200

    Added libstudio_file.

---
+++ libstudio_file/src/file.rs
@@ -0,0 +1,21 @@
+use std::pin::Pin;
+
+use async_std::prelude::*;
+use async_std::path::Path;
+use async_std::fs::File;
+
+pub struct FileHandle {
+    pub file: File,
+    pub data: Pin<Box<[u8]>>,
+}
+
+impl FileHandle {
+    pub async fn new(at: &Path) -> std::io::Result<Self> {
+        let mut file = File::open(at).await?;
+
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf).await?;
+
+        Ok(Self { file, data: Box::into_pin(buf.into_boxed_slice()) })
+    }
+}
---
+++ libstudio_file/src/lib.rs
@@ -0,0 +1,3 @@
+#![feature(box_into_pin)]
+
+mod file;
